85b5de62134b62ff517fc1416c4b6905769c784b
Tuyisenge2 <tuyisengetito3@gmail.com>
2025-04-08 16:55:05 +0200

    ft-insert center modal in create scene

--- lib/pages/scene/create_scene.dart
+++ lib/pages/scene/create_scene.dart
@@ -262,7 +262,14 @@ class _CreateScene extends State<CreateScene> {
               fontWeight: FontWeight.w700,
             ),
           ),
-          SvgPicture.asset("assets/icons/less2.svg", height: 16, width: 16),
+          InkWell(
+            onTap: () => _dialogBuilder(context),
+            child: SvgPicture.asset(
+              "assets/icons/less2.svg",
+              height: 16,
+              width: 16,
+            ),
+          ),
         ],
       ),
     );
@@ -372,6 +379,43 @@ class _CreateScene extends State<CreateScene> {
               ),
             ],
           ),
+        );
+      },
+    );
+  }
+
+  Future<void> _dialogBuilder(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Basic dialog title'),
+          content: const Text(
+            'A dialog is a type of modal window that\n'
+            'appears in front of app content to\n'
+            'provide critical information, or prompt\n'
+            'for a decision to be made.',
+          ),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Disable'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Enable'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
         );
       },
     );
